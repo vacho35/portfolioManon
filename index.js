2aec45c2258c369aa8fde0b700fb57c65402575b
vacho <quentinpoker1006@gmail.com>
2023-04-24 18:30:26 +0200

    modif title gallery

---
+++ index.js
@@ -0,0 +1,71 @@
+function smoothScroll1() {
+  var sec2 = document.getElementById("sec2");
+  sec2.scrollIntoView();
+  sec2.style.transitionTimingFunction = "ease-in";
+}
+
+function smoothScroll2() {
+  var sec3 = document.getElementById("sec3");
+  sec3.scrollIntoView();
+  sec3.style.transitionTimingFunction = "ease-in";
+}
+function smoothScroll3() {
+  var sec4 = document.getElementById("sec4");
+  sec4.scrollIntoView();
+  sec4.style.transitionTimingFunction = "ease-in";
+}
+function smoothScroll4() {
+  var sec5 = document.getElementById("sec5");
+  sec5.scrollIntoView();
+  sec5.style.transitionTimingFunction = "ease-in";
+}
+function smoothScroll5() {
+  var sec1 = document.getElementById("sec1");
+  sec1.scrollIntoView();
+  sec1.style.transitionTimingFunction = "ease-in";
+}
+
+////////////////////////////////         Menu Burger              /////////////////////////
+
+function openMenuMobile() {
+  document.querySelector("header").classList.add("open");
+  document.querySelector(".overlay-mobile").classList.add("open");
+}
+
+function closeMenuMobile() {
+  document.querySelector("header").classList.remove("open");
+  document.querySelector(".overlay-mobile").classList.remove("open");
+}
+
+
+////////////////////////////////         Gallery              /////////////////////////
+
+
+let allGridItems = [...document.getElementsByClassName("grid-item")];
+let popupBg = document.getElementById("popup-bg");
+let popupImg = document.getElementById("popup-img");
+
+const openPopup = (e) => {
+    let clickedImageSrc = e.target.src;
+    popupBg.classList.add("active");
+    popupImg.src = clickedImageSrc;
+};
+
+const closePopup = () => {
+    popupBg.classList.remove("active");
+};
+
+allGridItems.forEach((el) => el.addEventListener("click", openPopup));
+
+popupImg.addEventListener("click", (e) => e.stopPropagation());
+popupBg.addEventListener("click", closePopup);
+
+const items = document.querySelectorAll('img');
+const nbSlide = items.length;
+const suivant = document.querySelector('.right');
+const precedent = document.querySelector('.left');
+let count = 0;
+
+function slideSuivante(){
+  items[counts].classlist.remove
+}
