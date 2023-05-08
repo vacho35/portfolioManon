f5cf4b9c643387816e84dc192a47099de40fb299
vacho <quentinpoker1006@gmail.com>
2023-05-08 04:45:37 +0200

    slider pour les 4 pages

--- index.js
+++ index.js
@@ -39,39 +39,163 @@ function closeMenuMobile() {
 
 ////////////////////////////////         Gallery              /////////////////////////
 
-let allGridItems = [...document.getElementsByClassName("grid-item")];
-let popupBg = document.getElementById("popup-bg");
-let popupImg = document.getElementById("popup-img");
+const ornithologyImages = [...document.querySelectorAll(".ornithology")];
+const landscapesImages = [...document.querySelectorAll(".landscapes")];
+const archeologyImages = [...document.querySelectorAll(".archeology")];
+const animalsImages = [...document.querySelectorAll(".animals")];
 
-const openPopup = (e) => {
-  let clickedImageSrc = e.target.src;
-  popupBg.classList.add("active");
-  popupImg.src = clickedImageSrc;
-};
+// popup
+const popup = document.querySelector(".popup");
+const closeBtn = document.querySelector(".close-btn");
+const imageName = document.querySelector(".image-name");
+const largeImage = document.querySelector(".large-image");
+const imageIndex = document.querySelector(".index");
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+const galleryImage = document.querySelector(".gallery-image");
+const tableauOrnithology = ["Martin-pêcheur d'Europe", "Bernaches cravants", "Canard colvert", "Canard souchet", "Aigrette", "Aigrette", "Aigrette", "Bergeronette grise", "Bouvreuil pivoine", "Chardonneret élégant", "Chevalier"];
+const tableauLandscapes = ["Puy Marie (Auvergne)", "Puy-de-dome (Auvergne)", "Bretagne", "Bretagne", "Bretagne", "Bourges", "Etang"];
+const tableauArcheology = ["Carnac", "Krusty", "girouette", "violon"];
+const tableauAnimals = ["Chevres", "Biches", "Biches2", "Phoque", "Phoque2", "Phoque3"];
+const overlay = document.querySelector(".overlay");
+const valueBird = document.querySelector("value-Bird");
 
-const closePopup = () => {
-  popupBg.classList.remove("active");
-};
+let index = 0; // will track our current image;
 
-allGridItems.forEach((el) => el.addEventListener("click", openPopup));
+///////////////////////////////////    Slider Ornithology   /////////////////////////////////
 
-// popupImg.addEventListener("click", (e) => e.stopPropagation());
-// popupBg.addEventListener("click", closePopup);
+ornithologyImages.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    updateOrnithologyImage(i);
+    popup.classList.toggle("active");
+    overlay.classList.toggle("open");
+  });
+});
+function updateOrnithologyImage(i) {
+  let path = `ressources/images/ornithology/img${i + 1}.png`;
+  largeImage.src = path;
+  imageName.innerHTML = tableauOrnithology[i];
+  index = i;
+}
+function closePopup() {
+  popup.classList.remove("active");
+  overlay.classList.remove("open");
+}
+function imagePrecedente() {
+  if (index > 0) {
+    updateOrnithologyImage(index - 1);
+  }
+}
+function imageSuivante() {
+  if (index < ornithologyImages.length - 1) {
+    updateOrnithologyImage(index + 1);
+  }
+}
 
-////////////////////////////////         Slider             /////////////////////////
 
-const slide = ["Carnac.JPG", "DSC_0191.JPG", "DSC_0196.JPG"];
+///////////////////////////////////    Slider Landcapes   /////////////////////////////////
+
+
+landscapesImages.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    updateLandscapesImage(i);
+    popup.classList.toggle("active");
+    overlay.classList.toggle("open");
+  });
+});
+function updateLandscapesImage(i) {
+  let path = `ressources/images/landscapes/img${i + 1}.png`;
+  largeImage.src = path;
+  imageName.innerHTML = tableauLandscapes[i];
+  index = i;
+}
+function closePopup() {
+  popup.classList.remove("active");
+  overlay.classList.remove("open");
+}
+function imagePrecedente() {
+  if (index > 0) {
+    updateLandscapesImage(index - 1);
+  }
+}
+function imageSuivante() {
+  if (index < landscapesImages.length - 1) {
+    updateLandscapesImage(index + 1);
+  }
+}
+
+
+///////////////////////////////////    Slider Archeology   /////////////////////////////////
+
+
+archeologyImages.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    updateArcheologyImage(i);
+    popup.classList.toggle("active");
+    overlay.classList.toggle("open");
+  });
+});
+function updateArcheologyImage(i) {
+  let path = `ressources/images/archeology/img${i + 1}.png`;
+  largeImage.src = path;
+  imageName.innerHTML = tableauArcheology[i];
+  index = i;
+}
+function closePopup() {
+  popup.classList.remove("active");
+  overlay.classList.remove("open");
+}
+function imagePrecedente() {
+  if (index > 0) {
+    updateArcheologyImage(index - 1);
+  }
+}
+function imageSuivante() {
+  if (index < archeologyImages.length - 1) {
+    updateArcheologyImage(index + 1);
+  }
+}
+
+///////////////////////////////////    Slider Animals   /////////////////////////////////
+
+animalsImages.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    updateAnimalsImage(i);
+    popup.classList.toggle("active");
+    overlay.classList.toggle("open");
+  });
+});
+function updateAnimalsImage(i) {
+  let path = `ressources/images/animals/img${i + 1}.png`;
+  largeImage.src = path;
+  imageName.innerHTML = tableauAnimals[i];
+  index = i;
+}
+function closePopup() {
+  popup.classList.remove("active");
+  overlay.classList.remove("open");
+}
+function imagePrecedente() {
+  if (index > 0) {
+    updateAnimalsImage(index - 1);
+  }
+}
+function imageSuivante() {
+  if (index < animalsImages.length - 1) {
+    updateAnimalsImage(index + 1);
+  }
+}
+
+////////////////////////////////         Dark Mode             /////////////////////////
+
 const imgs = document.querySelectorAll("img");
 const nbSlide = imgs.length;
-let numero = 0;
+const suivant = document.querySelector(".right");
+const precedent = document.querySelector(".left");
+let count = 0;
 
-function ChangeSlide(sens) {
-  numero = numero + sens;
-  if (numero < 0 )
-    numero = nbSlide - 1;
-  if (numero > nbSlide - 1)
-  numero = 0;
-  document.getElementById("nbSlide").src = nbSlide[numero];
+function slideSuivante() {
+  imgs[counts].classlist.remove;
 }
 
 ////////////////////////////////         Dark Mode             /////////////////////////
@@ -80,15 +204,11 @@ const body = document.querySelector("body");
 const toggleMode = document.querySelector(".theme-switch");
 
 toggleMode.addEventListener("click", () => {
-  if(body.classList.contains('dark')){
-
-    body.classList.add('light')
-    body.classList.remove('dark')
-
-} else if(body.classList.contains('light')){
-
-    body.classList.add('dark')
-    body.classList.remove('light')
-
-}
+  if (body.classList.contains("dark")) {
+    body.classList.add("light");
+    body.classList.remove("dark");
+  } else if (body.classList.contains("light")) {
+    body.classList.add("dark");
+    body.classList.remove("light");
+  }
 });
